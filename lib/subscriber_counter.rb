72dc8e76ad2ed49de86a68b5a5d9e89cb1f95906
Gregory Brown <gregory.t.brown@gmail.com>
2011-11-16 00:10:29 -0500

    fairly nice mailchimp separation

--- lib/subscriber_counter.rb
+++ lib/subscriber_counter.rb
@@ -16,28 +16,15 @@ class SubscriberCounter
 
   include Enumerable
 
-  def initialize(weeks_of_context)
-    api = Hominid::API.new(MailChimp::SETTINGS[:api_key])
-
-    campaign_data = api.campaigns["data"]
-                       .map { |e| [ e["id"], Date.parse(e["send_time"])] }
-                       .sort_by { |id,date| date }
-
-    campaign_count = campaign_data.size
+  def initialize(data, weeks_of_context)
+    campaign_count = data.size
         
     @data = (campaign_count-weeks_of_context).upto(campaign_count-1).map do |i|
-      campaign_id, date = campaign_data[i]
-      subscribers = api.campaign_segment_test(MailChimp::SETTINGS[:list_id], 
-        :match      => "all", 
-        :conditions => [{:field => "date", :op => "lt", :value => campaign_id},
-                        {:field => "interests-#{MailChimp::SETTINGS[:grouping_id]}", 
-                         :op    => "none", 
-                         :value => "Free Subscription"}]
-      )
+      campaign = data[i]
 
       WeekStats.new(:issue_number     => i+1, 
-                    :date             => date.strftime("%Y.%m.%d"), 
-                    :subscriber_count => subscribers)
+                    :date             => campaign.date.strftime("%Y.%m.%d"), 
+                    :subscriber_count => campaign.subscriber_count)
     end
   end
 
@@ -46,8 +33,47 @@ class SubscriberCounter
   end
 end
 
+class Campaign
+  include Comparable
+
+  def self.all
+    api = Hominid::API.new(MailChimp::SETTINGS[:api_key])
+
+    api.campaigns(:list_id => MailChimp::SETTINGS[:list_id])["data"].map { |e|
+      new(:api => api, :id => e["id"], :date => Date.parse(e["send_time"]))
+    }.sort 
+  end
+
+  def initialize(params={})
+    @api  = params.fetch(:api)
+    @id   = params.fetch(:id)
+    @date = params.fetch(:date)
+  end
+
+  attr_reader :date
+
+  def <=>(other)
+    date <=> other.date
+  end
+
+  def subscriber_count
+    api.campaign_segment_test(MailChimp::SETTINGS[:list_id], 
+      :match      => "all", 
+      :conditions => [{:field => "date", :op => "lt", :value => id},
+                      {:field => "interests-#{MailChimp::SETTINGS[:grouping_id]}", 
+                       :op    => "none", 
+                       :value => "Free Subscription"}]
+    )
+  end
+
+  private 
+
+  attr_reader :api, :id
+end
+
+
 =begin
-counter = SubscriberCounter.new(12)
+counter = SubscriberCounter.new(Campaign.all, 6)
 
 p counter.map { |e| [e.issue_number, e.subscriber_count] }
 =end
