270a6b4d122c66a0855b2ffbae644c713dbeaf4d
Gregory Brown <gregory.t.brown@gmail.com>
2011-11-16 20:56:55 -0500

    Make into a bin file

--- example.rb
+++
@@ -1,8 +0,0 @@
-require_relative "lib/subscription_counter"
-
-campaigns = SubscriptionCounter::Campaign.all
-
-counter   = SubscriptionCounter.new(campaigns, 10)
-report    = SubscriptionCounter::Report.new(counter)
-
-SubscriptionCounter::Report::PDF.new(report).save_as("foo.pdf")
