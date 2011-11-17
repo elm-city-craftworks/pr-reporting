5bc190e96210afd0e260ffa7ad62418435c67d3d
Gregory Brown <gregory.t.brown@gmail.com>
2011-11-16 21:51:39 -0500

    Use R to compute stddev and mean

--- lib/subscription_counter/statistics.rb
+++ lib/subscription_counter/statistics.rb
@@ -2,28 +2,15 @@ module SubscriptionCounter
   Statistics = Object.new
 
   class << Statistics 
-    def mean(list)
-      list.reduce(:+) / list.length.to_f
-    end
-
-    def sample_variance(list)
-      avg = mean(list)
-      sum = list.reduce(0) { |acc,i| acc + (i-avg)**2 }
-
-      return sum / (list.length.to_f - 1)
-    end
-
-    def standard_deviation(list)
-      Math.sqrt(sample_variance(list))
-    end
-
     # Take a list and compute its standard deviation
     # and mean, and then uses this information to reject
     # outliers from the original list. Finally, compute
     # the mean of the adjusted list.
     def adjusted_mean(list)
-      mean    = mean(list)
-      std_dev = standard_deviation(list)
+      r = RSRuby.instance
+
+      mean    = r.mean(list)
+      std_dev = r.sd(list)
 
       lower_limit = mean - std_dev
       upper_limit = mean + std_dev
@@ -32,14 +19,7 @@ module SubscriptionCounter
         (lower_limit..upper_limit).include?(e)
       end
 
-      mean(list_without_outliers)
-    end
-
-    # Takes a list and computes the difference between
-    # each consecutive pair of values, returning a new
-    # list with the results
-    def deltas(list)
-      list.each_cons(2).map { |a,b| b - a }
+      r.mean(list_without_outliers)
     end
   end
 end
